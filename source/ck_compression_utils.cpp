50a9eb341b4e00e774c5a3bd91809310212b7a71
Tony Di Croce <tony@agrian.com>
2018-10-03 17:12:05 -0700

    added support for zero length buffers.

--- source/ck_compression_utils.cpp
+++ source/ck_compression_utils.cpp
@@ -32,88 +32,94 @@ typedef
 
 uint64_t cppkit::ck_compression_utils::compress_buffer(const uint8_t* src, uint64_t srcLen, uint8_t* dst, uint64_t dstLen)
 {
-    bz_stream bzs;
-    memset(&bzs, 0, sizeof(bz_stream));
-
-    BZ2_bzCompressInit(&bzs, 9, 0, 30);
-
-    // Before each call to BZ2_bzCompress, next_in should point at the data to be compressed, and avail_in should indicate how many bytes the library may read. BZ2_bzCompress updates 
-    // next_in, avail_in and total_in to reflect the number of bytes it has read.
-
-    auto s = const_cast<uint8_t*>(src);
-    auto d = dst;
-
     uint64_t totalIn = 0;
     uint64_t totalOut = 0;
 
-    int ret;
-    do
+    if(srcLen > 0)
     {
-        bzs.next_in = (char*)(s + totalIn);
-        bzs.avail_in = (srcLen >= UINT_MAX)?UINT_MAX:srcLen;
-        bzs.next_out = (char*)(d + totalOut);
-        bzs.avail_out = (dstLen >= UINT_MAX)?UINT_MAX:dstLen;
+        bz_stream bzs;
+        memset(&bzs, 0, sizeof(bz_stream));
 
-        ret = BZ2_bzCompress(&bzs, (srcLen >= UINT_MAX)?BZ_RUN:BZ_FINISH);
+        BZ2_bzCompressInit(&bzs, 9, 0, 30);
 
-        totalIn = (((uint64_t)bzs.total_in_hi32) << 32) + bzs.total_in_lo32;
-        srcLen -= totalIn;
-        totalOut = (((uint64_t)bzs.total_out_hi32) << 32) + bzs.total_out_lo32;
-        dstLen -= totalOut;
+        // Before each call to BZ2_bzCompress, next_in should point at the data to be compressed, and avail_in should indicate how many bytes the library may read. BZ2_bzCompress updates 
+        // next_in, avail_in and total_in to reflect the number of bytes it has read.
 
-        if(ret < 0)
+        auto s = const_cast<uint8_t*>(src);
+        auto d = dst;
+
+        int ret;
+        do
         {
-            BZ2_bzCompressEnd(&bzs);
-            CK_THROW(("bzip2 error: %d", ret));
-        }
-    }
-    while(ret != BZ_STREAM_END);
+            bzs.next_in = (char*)(s + totalIn);
+            bzs.avail_in = (srcLen >= UINT_MAX)?UINT_MAX:srcLen;
+            bzs.next_out = (char*)(d + totalOut);
+            bzs.avail_out = (dstLen >= UINT_MAX)?UINT_MAX:dstLen;
 
-    BZ2_bzCompressEnd(&bzs);
+            ret = BZ2_bzCompress(&bzs, (srcLen >= UINT_MAX)?BZ_RUN:BZ_FINISH);
+
+            totalIn = (((uint64_t)bzs.total_in_hi32) << 32) + bzs.total_in_lo32;
+            srcLen -= totalIn;
+            totalOut = (((uint64_t)bzs.total_out_hi32) << 32) + bzs.total_out_lo32;
+            dstLen -= totalOut;
+
+            if(ret < 0)
+            {
+                BZ2_bzCompressEnd(&bzs);
+                CK_THROW(("bzip2 error: %d", ret));
+            }
+        }
+        while(ret != BZ_STREAM_END);
+
+        BZ2_bzCompressEnd(&bzs);
+    }
 
     return totalOut;
 }
 
 uint64_t cppkit::ck_compression_utils::decompress_buffer(const uint8_t* src, uint64_t srcLen, uint8_t* dst, uint64_t dstLen)
 {
-    bz_stream bzs;
-    memset(&bzs, 0, sizeof(bz_stream));
-
-    BZ2_bzDecompressInit(&bzs, 0, 0);
-
-    // Before each call to BZ2_bzCompress, next_in should point at the data to be compressed, and avail_in should indicate how many bytes the library may read. BZ2_bzCompress updates 
-    // next_in, avail_in and total_in to reflect the number of bytes it has read.
-
-    auto s = const_cast<uint8_t*>(src);
-    auto d = dst;
-
     uint64_t totalIn = 0;
     uint64_t totalOut = 0;
 
-    int ret;
-    do
+    if(srcLen > 0)
     {
-        bzs.next_in = (char*)(s + totalIn);
-        bzs.avail_in = (srcLen >= UINT_MAX)?UINT_MAX:srcLen;
-        bzs.next_out = (char*)(d + totalOut);
-        bzs.avail_out = (dstLen >= UINT_MAX)?UINT_MAX:dstLen;
+        bz_stream bzs;
+        memset(&bzs, 0, sizeof(bz_stream));
 
-        ret = BZ2_bzDecompress(&bzs);
+        BZ2_bzDecompressInit(&bzs, 0, 0);
 
-        totalIn = (((uint64_t)bzs.total_in_hi32) << 32) + bzs.total_in_lo32;
-        srcLen -= totalIn;
-        totalOut = (((uint64_t)bzs.total_out_hi32) << 32) + bzs.total_out_lo32;
-        dstLen -= totalOut;
+        // Before each call to BZ2_bzCompress, next_in should point at the data to be compressed, and avail_in should indicate how many bytes the library may read. BZ2_bzCompress updates 
+        // next_in, avail_in and total_in to reflect the number of bytes it has read.
 
-        if(ret < 0)
+        auto s = const_cast<uint8_t*>(src);
+        auto d = dst;
+
+        int ret;
+        do
         {
-            BZ2_bzDecompressEnd(&bzs);
-            CK_THROW(("bzip2 error: %d", ret));
-        }
-    }
-    while(ret != BZ_STREAM_END);
+            bzs.next_in = (char*)(s + totalIn);
+            bzs.avail_in = (srcLen >= UINT_MAX)?UINT_MAX:srcLen;
+            bzs.next_out = (char*)(d + totalOut);
+            bzs.avail_out = (dstLen >= UINT_MAX)?UINT_MAX:dstLen;
 
-    BZ2_bzDecompressEnd(&bzs);
+            ret = BZ2_bzDecompress(&bzs);
+
+            totalIn = (((uint64_t)bzs.total_in_hi32) << 32) + bzs.total_in_lo32;
+            srcLen -= totalIn;
+            totalOut = (((uint64_t)bzs.total_out_hi32) << 32) + bzs.total_out_lo32;
+            dstLen -= totalOut;
+
+            if(ret < 0)
+            {
+                BZ2_bzDecompressEnd(&bzs);
+                CK_THROW(("bzip2 error: %d", ret));
+            }
+        }
+        while(ret != BZ_STREAM_END);
+
+        BZ2_bzDecompressEnd(&bzs);
+    }
 
     return totalOut;
 }
